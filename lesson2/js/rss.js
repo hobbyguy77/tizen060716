1bb4d500591beb80a1aa3b20b9808ae86414b919
hobbyguy77 <markmak77@gmail.com>
2016-07-06 17:08:50 +0300

    mySQL

--- lesson2/js/rss.js
+++ lesson2/js/rss.js
@@ -3,142 +3,177 @@
  */
 function getFeed() {
 	var FEED_URL = "http://www.3dnews.ru/news/rss/";
+	var baseName = "filesBase3",	storeName = "filesStore";
 	var width = screen.width;
-	$(document).ready(function() {
-		$.ajax({
-			type: "GET",
-			url: FEED_URL,
-			dataType: "xml",
-			error: getStorage(function(res){
-				for(var field in res){
-					for(var fieldVal in (value = res[field])){
-						switch(fieldVal){
-						case 'title':
-							var title = value[fieldVal];
-							break;
-						case 'description':
-							var description = value[fieldVal];
-							break;
-						case 'url_img':
-							var url_img = value[fieldVal];
-							break;
-						}
-					}
-					$("#rssContent").append('<div class="feed"><div class="image"><img src=' + url_img + ' width=' + width +'px /></div><div class="title">' + title + '</div><div class="description">' + description + '</div></div>');
-				}
-				
-			}),
-			success: xmlParser
-		});
+	var db = openDatabase(baseName, '1', 'Test_DB', 200000);
+	
+	db.transaction(function (tx){
+		tx.executeSql('CREATE TABLE IF NOT EXISTS '+storeName+' (title TEXT, description TEXT, image TEXT)', [], null, function(e){console.log(e)});
 	});
 
-	function xmlParser(xml) {
-		var arr = [];
-		var i = 0;
-		clearStorage();
-		$(xml).find("item").each(
-				function() {
-					var url_img = $(this).find("enclosure").attr("url");
-					var title = $(this).find("title").text();
-					var description = $(this).find("description").text();
-					
-					$("#rssContent").append('<div class="feed"><div class="image"><img src=' + url_img + ' width=' + width +'px /></div><div class="title">' + title + '</div><div class="description">' + description + '</div></div>');
-					
-					
-					arr[i] = {url_img:url_img, title:title, description:description};
-					setData(arr[i]);
-					i++;
+	$(document).ready(
+			function() {
+				$.ajax({
+					type : "GET",
+					url : FEED_URL,
+					dataType : "xml",
+//					error : getStorage(function(res) {
+//						for ( var field in res) {
+//							for ( var fieldVal in (value = res[field])) {
+//								switch (fieldVal) {
+//								case 'title':
+//									var title = value[fieldVal];
+//									break;
+//								case 'description':
+//									var description = value[fieldVal];
+//									break;
+//								case 'url_img':
+//									var url_img = value[fieldVal];
+//									break;
+//								}
+//							}
+//							$("#rssContent").append(
+//									'<div class="feed"><div class="image"><img src='
+//											+ url_img + ' width=' + width
+//											+ 'px /></div><div class="title">'
+//											+ title
+//											+ '</div><div class="description">'
+//											+ description + '</div></div>');
+//						}
+//
+//					}),
+					success : xmlParser
 				});
+			});
+
+	function xmlParser(xml) {
+		//var arr = [];
+		//var i = 0;
+		//clearStorage();
+		db.transaction(function (tx){
+			tx.executeSql('DROP TABLE ' + storeName, [], null, function(e){console.log('drop error')});
+			tx.executeSql('CREATE TABLE IF NOT EXISTS '+storeName+' (title TEXT, description TEXT, image TEXT)', [], null, function(e){console.log('recreate error')});
+			console.log('cleared DB');
+		});
+		$(xml)
+				.find("item")
+				.each(
+						function() {
+							var url_img = $(this).find("enclosure").attr("url");
+							var title = $(this).find("title").text();
+							var description = $(this).find("description")
+									.text();
+
+							$("#rssContent").append(
+									'<div class="feed"><div class="image"><img src='
+											+ url_img + ' width=' + width
+											+ 'px /></div><div class="title">'
+											+ title
+											+ '</div><div class="description">'
+											+ description + '</div></div>');
+							
+							db.transaction(function (tx){
+								tx.executeSql('INSERT INTO '+storeName+' (title, description, image) VALUES (?, ?, ?)', [title, description, url_img], null, function(){console.log("insert error")});
+							});
+
+// arr[i] = {
+// url_img : url_img,
+// title : title,
+// description : description
+// };
+							// setData(arr[i]);
+							//i++;
+						});
 	}
 }
 
+/*var indexedDB = window.indexedDB || window.mozIndexedDB
+		|| window.webkitIndexedDB || window.msIndexedDB, 
+		IDBTransaction = window.IDBTransaction
+		|| window.webkitIDBTransaction || window.msIDBTransaction, 
+		baseName = "filesBase", 
+		storeName = "filesStore";
 
-
-
-
-var indexedDB 	  = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB,
-IDBTransaction  = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction,
-baseName 	  = "filesBase",
-storeName 	  = "filesStore";
-
-
-
-
-function logerr(err){
+function logerr(err) {
 	console.log(err);
 }
 
-function connectDB(f){
+function connectDB(f) {
 	var request = indexedDB.open(baseName, 1);
 	request.onerror = logerr;
-	request.onsuccess = function(){
+	request.onsuccess = function() {
 		f(request.result);
 	}
-	request.onupgradeneeded = function(e){
-		var objectStore = e.currentTarget.result.createObjectStore(storeName, { autoIncrement: true });
+	request.onupgradeneeded = function(e) {
+		var objectStore = e.currentTarget.result.createObjectStore(storeName, {
+			autoIncrement : true
+		});
 		connectDB(f);
 	}
 }
 
-function getData(key, f){
-	connectDB(function(db){
-		var request = db.transaction([storeName], "readonly").objectStore(storeName).get(key);
+function getData(key, f) {
+	connectDB(function(db) {
+		var request = db.transaction([ storeName ], "readonly").objectStore(
+				storeName).get(key);
 		request.onerror = logerr;
-		request.onsuccess = function(){
+		request.onsuccess = function() {
 			f(request.result ? request.result : -1);
 		}
 	});
 }
 
-function getStorage(f){
-	connectDB(function(db){
-		var rows = [],
-			store = db.transaction([storeName], "readonly").objectStore(storeName);
+function getStorage(f) {
+	connectDB(function(db) {
+		var rows = [], store = db.transaction([ storeName ], "readonly")
+				.objectStore(storeName);
 
-		if(store.mozGetAll)
-			store.mozGetAll().onsuccess = function(e){
+		if (store.mozGetAll)
+			store.mozGetAll().onsuccess = function(e) {
 				f(e.target.result);
 			};
 		else
 			store.openCursor().onsuccess = function(e) {
 				var cursor = e.target.result;
-				if(cursor){
+				if (cursor) {
 					rows.push(cursor.value);
 					cursor.continue();
-				}
-				else {
+				} else {
 					f(rows);
 				}
 			};
 	});
 }
 
-function setData(obj){
-	connectDB(function(db){
-		var request = db.transaction([storeName], "readwrite").objectStore(storeName).add(obj);
+function setData(obj) {
+	connectDB(function(db) {
+		var request = db.transaction([ storeName ], "readwrite").objectStore(
+				storeName).add(obj);
 		request.onerror = logerr;
-		request.onsuccess = function(){
+		request.onsuccess = function() {
 			return request.result;
 		}
 	});
 }
 
-function delData(key){
-	connectDB(function(db){
+function delData(key) {
+	connectDB(function(db) {
 		var request = db.transaction([storeName], "readwrite").objectStore(storeName).delete(key);
 		request.onerror = logerr;
-		request.onsuccess = function(){
+		request.onsuccess = function() {
 			console.log("File deleted from DB:", file);
 		}
 	});
 }
 
-function clearStorage(){
-	connectDB(function(db){
-		var request = db.transaction([storeName], "readwrite").objectStore(storeName).clear();;
+function clearStorage() {
+	connectDB(function(db) {
+		var request = db.transaction([ storeName ], "readwrite").objectStore(
+				storeName).clear();
+		;
 		request.onerror = logerr;
-		request.onsuccess = function(){
+		request.onsuccess = function() {
 			console.log("Clear");
 		}
 	});
-}
+}*/
